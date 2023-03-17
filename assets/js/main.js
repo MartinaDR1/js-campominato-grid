31c8848e1a316ba749dfff28fcc07832690b7705
MartinaDR1 <derosemartina933@gmail.com>
2023-03-17 23:05:15 +0100

    add footer + details header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,39 @@
 // Seleziono elementi della DOM
 const btnSend = document.querySelector('button');
 const containerEl = document.querySelector('.container');
-const selectEl = document.querySelector('.select')
+const selectEl = document.getElementById('select');
+const footerEl = document.querySelector('footer')
 // Rendo il bottone cliccabile e creo la tabella
 
 /*let maxCells = 100;*/
 
 btnSend.addEventListener('click', function(){
+    containerEl.innerHTML = '';
 
-    if(selectEl == 'easy'){
+    if(selectEl.value == 'easy'){
         cellContainer (100, 'easy')
-    } else if (selectEl == 'medium'){
+    } else if (selectEl.value == 'medium'){
         cellContainer(81, 'medium')
     } else {
         cellContainer (49, 'hard')
     }
+
+    footerEl.innerHTML = "<span> Made with &hearts; by Boolean</span>";
+    footerEl.style.padding='2rem'
+   
+
 })
 
 function cellContainer (maxCells, difficulty){
     
     for(let i=1; i <= maxCells; i++){
-        const cellDom = `<div class="cell">${i}</div>`
+        const cellDom = `<div class="cell ${difficulty}">${i}</div>` 
         containerEl.innerHTML += cellDom
-
     }
 
     //Seleziono tutte le celle
     const cells = document.querySelectorAll('.cell');
-
+   
     // Seleziono ogni cella
     for (let i = 0; i < cells.length; i++) {
         
@@ -36,7 +42,7 @@ function cellContainer (maxCells, difficulty){
         //Aggiungo un event listener al click
         thisCell.addEventListener('click', function() {
             thisCell.classList.toggle('bg_active');
-            console.log(`hai cliccato la casella ${thisCell.innerHTML}`);
+            console.log(`Hai cliccato la casella ${thisCell.innerHTML}`);
         })
     }
 }
